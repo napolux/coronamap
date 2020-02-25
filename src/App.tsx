32a8fe7163ac07caadb12501d55b5f77824b4510
Napolux <napolux@gmail.com>
2020-02-25 20:15:02 +0100

    Improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Map, TileLayer, CircleMarker, Popup } from "react-leaflet";
+import { Map, TileLayer, CircleMarker, Popup, ZoomControl, LayerGroup } from "react-leaflet";
 import "./App.css";
 
 interface DataMarker {
@@ -13,20 +13,28 @@ export default function App() {
 
   const data: DataMarker[] = require('./data.json');
   return (
-    <div className="mapContainer">
-      <Map center={[41.90, 12.50]} zoom={5}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {data.map(e => (
-          <CircleMarker center={[e.coords.lat, e.coords.lng]} fillColor="red" color="red" radius={(Math.log(e.cases) * 5) + 5}>
-            <Popup><h3>{e.region}</h3><strong>Casi:</strong> {e.cases} <br /><strong>Morti:</strong> {e.deads}</Popup>
-          </CircleMarker>
-        ))}
-      </Map>
-      <div className="ideaContainer">Un'idea di <a rel="noopener noreferrer" href="https://napolux.com" target="_blank">Francesco Napoletano</a></div>
-    </div>
-  );
+    <>
+      <div className="ideaContainer">
+        <h3>COVID19 Italia: 25 febbraio 2020, ore 18.00</h3>
+        <p>Un'idea di <a rel="noopener noreferrer" href="https://napolux.com?utm_source=coronamap" target="_blank">Francesco Napoletano</a></p>
+        <p>Sorgente dati: <a rel="noopener noreferrer" target="_blank" href="http://www.salute.gov.it/nuovocoronavirus">salute.gov.it</a></p>
+      </div>
+      <div className="mapContainer">
+        <Map center={[41.90, 12.50]} zoom={5} zoomControl={false}>
+        <ZoomControl position="bottomleft" />
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <LayerGroup>
+            {data.map(e => (
+              <CircleMarker center={[e.coords.lat, e.coords.lng]} fillColor="red" color="red" radius={(Math.log(e.cases) * 5) + 5}>
+                <Popup><h3>{e.region}</h3><strong>Casi:</strong> {e.cases} <br /><strong>Morti:</strong> {e.deads}</Popup>
+              </CircleMarker>
+            ))}
+          </LayerGroup>
+        </Map>
+      </div>
+    </>);
 }
 
