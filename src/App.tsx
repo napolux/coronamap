f9207f835d5da71d363615f24d05e06e7e6e5dc9
Napolux <napolux@gmail.com>
2020-02-25 19:24:05 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Map, TileLayer, CircleMarker, Popup } from "react-leaflet";
+import "./App.css";
 
-function App() {
+interface DataMarker {
+  "region": string;
+  "coords": { "lat": number, "lng": number };
+  "cases": number;
+  "deads": number;
+}
+
+export default function App() {
+
+  const data: DataMarker[] = require('./data.json');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mapContainer">
+      <Map center={[41.90, 12.50]} zoom={5}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {data.map(e => (
+          <CircleMarker center={[e.coords.lat, e.coords.lng]} fillColor="red" color="red" radius={(Math.log(e.cases) * 5) + 5}>
+            <Popup><h3>{e.region}</h3><strong>Casi:</strong> {e.cases} <br /><strong>Morti:</strong> {e.deads}</Popup>
+          </CircleMarker>
+        ))}
+      </Map>
+      <div className="ideaContainer">Un'idea di <a rel="noopener noreferrer" href="https://napolux.com" target="_blank">Francesco Napoletano</a></div>
     </div>
   );
 }
 
-export default App;
