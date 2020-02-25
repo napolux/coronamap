1ed299cd6eb0baeb174e9f62a12e77247f11dce7
Napolux <napolux@gmail.com>
2020-02-25 20:26:41 +0100

    adding relative paths

---
+++ build/precache-manifest.18e00a278f0ed5042fe29840c131a013.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "014fef377839bff2579ff0659535515e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a2a90559f34a15e17d9",
+    "url": "./static/css/main.492ff618.chunk.css"
+  },
+  {
+    "revision": "5b9c4d1da88e81dadedb",
+    "url": "./static/js/2.31a2f45f.chunk.js"
+  },
+  {
+    "revision": "ac5d9a2ccd3ba848587d2a460762f2f0",
+    "url": "./static/js/2.31a2f45f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a2a90559f34a15e17d9",
+    "url": "./static/js/main.0560e0b0.chunk.js"
+  },
+  {
+    "revision": "143adcfe3a5767a72d45",
+    "url": "./static/js/runtime-main.55868e15.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c5798d4ef45428c34475d26f9018758f.js"
+  "./precache-manifest.18e00a278f0ed5042fe29840c131a013.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
