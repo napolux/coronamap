1996f6e43ed57ab6f921da616877336fa9a2fff1
Napolux <napolux@gmail.com>
2020-03-01 19:22:04 +0100

    Update

---
+++ build/precache-manifest.9f9f18ae3c72f7c410ae5a5576940fcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e198aa130ba9b83c128e28b24852e306",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46dea93055c797fe0926",
+    "url": "./static/css/main.d5af2bb6.chunk.css"
+  },
+  {
+    "revision": "5b9c4d1da88e81dadedb",
+    "url": "./static/js/2.31a2f45f.chunk.js"
+  },
+  {
+    "revision": "ac5d9a2ccd3ba848587d2a460762f2f0",
+    "url": "./static/js/2.31a2f45f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46dea93055c797fe0926",
+    "url": "./static/js/main.e885cf82.chunk.js"
+  },
+  {
+    "revision": "143adcfe3a5767a72d45",
+    "url": "./static/js/runtime-main.55868e15.js"
+  }
+]);
