8107791024839ab2411119f7cd25a43ce6403d96
Napolux <napolux@gmail.com>
2020-03-03 19:05:16 +0100

    Update

--- build/precache-manifest.ea210f52c05cb4ed447689047ff95215.js
+++ build/precache-manifest.ea210f52c05cb4ed447689047ff95215.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7ba762525d6d8228542023003c4f03ed",
+    "revision": "fa6882fb79625c6dfbe638c5699c3520",
     "url": "./index.html"
   },
   {
-    "revision": "35fe8435c62318e5cf7b",
+    "revision": "3b8ad1e98b74ed9be550",
     "url": "./static/css/main.d5af2bb6.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.31a2f45f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "35fe8435c62318e5cf7b",
-    "url": "./static/js/main.57c5dce9.chunk.js"
+    "revision": "3b8ad1e98b74ed9be550",
+    "url": "./static/js/main.3010e5ca.chunk.js"
   },
   {
     "revision": "143adcfe3a5767a72d45",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fc8ca7e770a1cff6acddab66c3ccd83d.js"
+  "./precache-manifest.ea210f52c05cb4ed447689047ff95215.js"
 );
 
 self.addEventListener('message', (event) => {
