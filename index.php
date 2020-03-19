3a0d4aa231b78c5f6e2fa0786c5a3868a8834bd4
Napolux <napolux@gmail.com>
2020-03-19 18:30:29 +0100

    Ciaone

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+
+header('Location: http://opendatadpc.maps.arcgis.com/apps/opsdashboard/index.html#/b0c68bce2cce478eaac82fe38d4138b1');
--- src/App.test.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-test('this is fake', () => {
-  expect(true).toEqual(true);
-});
